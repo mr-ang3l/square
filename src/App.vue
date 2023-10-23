71420f5ec2c12230e53aead3f017f50577e5b1c6
Ángel De Ávila <62043490+mr-ang3l@users.noreply.github.com>
2023-10-23 15:05:37 +0000

    "start vew shifted for preview"

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-// import Start from './views/start-page.vue'
+import Start from './views/start-page.vue'
 
 import Profile from './views/profile-page.vue'
 </script>
 
 <template>
-  <Profile />
+  <Start />
 </template>
